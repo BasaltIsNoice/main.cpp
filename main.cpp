c717a8e449c53890aed8c35cc80da36fb6ee1060
LimMinYee <166193537+BasaltIsNoice@users.noreply.github.com>
2024-04-20 12:04:57 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << " Please enter your name. " << endl;
+  string name;
+  cin >> name;
+  cout << " Hello, " << name << "." << " Your birthday is on the 8th of July. " << endl;
+  return 0;
+}
